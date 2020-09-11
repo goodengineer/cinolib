5bde968b2f7af3a673516648f7dc919d8e749e83
P.E.Mazniker <60963077+goodengineer@users.noreply.github.com>
2020-09-11 17:53:25 +0300

    Update main.cpp
    propos.

--- examples/13_polyhedral_mesh_generation/main.cpp
+++ examples/13_polyhedral_mesh_generation/main.cpp
@@ -17,22 +17,24 @@
 #include <cinolib/profiler.h>
 
 //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+using namespace cinolib;
+using namespace std;
 
 int main(int argc, char **argv)
 {
-    using namespace cinolib;
-
+    
     QApplication app(argc, argv);
 
-    std::string s = (argc==2) ? std::string(argv[1]) : std::string(DATA_PATH) + "/3holes.obj";
+    string s = (argc==2) ? string(argv[1]) : string(DATA_PATH) + "/3holes.obj";
     DrawableTrimesh<> m_in(s.c_str());
 
     // make tetmesh
-    std::vector<uint>   edges, tets;
-    std::vector<double> verts;
-    double vol_thresh = 0.01 * m_in.bbox().diag(); // force tets to be smaller than 5% of bbox diag
+    vector<uint>   edges, tets;
+    vector<float> verts;
+    float vol_thresh = 0.01 * m_in.bbox().diag(); // force tets to be smaller than 5% of bbox diag
     char opt[100];
     sprintf(opt, "YQqa%f", vol_thresh);
+    
     tetgen_wrap(serialized_xyz_from_vec3d(m_in.vector_verts()), serialized_vids_from_polys(m_in.vector_polys()), edges, opt, verts, tets);
     DrawableTetmesh<> m_tet(verts, tets);
 
@@ -61,7 +63,7 @@ int main(int argc, char **argv)
     gui.resize(800,600);
     m_in.show_wireframe(true);
 
-    std::string pov("0.217281 -0.976103 0.00348926 0 0.94495 0.209448 -0.2514 0 0.244662 0.0579212 0.967876 0 -0.747865 0.366815 -2.40522 1 1.32704 2.47434");
+    string pov("0.217281 -0.976103 0.00348926 0 0.94495 0.209448 -0.2514 0 0.244662 0.0579212 0.967876 0 -0.747865 0.366815 -2.40522 1 1.32704 2.47434");
     gui_in.deserialize_POV(pov);
     gui_tet.deserialize_POV(pov);
     gui_poly.deserialize_POV(pov);
@@ -82,7 +84,6 @@ int main(int argc, char **argv)
     QApplication::connect(new QShortcut(QKeySequence(Qt::CTRL+Qt::Key_1), &gui_in),   &QShortcut::activated, [&](){panel_in.show();});
     QApplication::connect(new QShortcut(QKeySequence(Qt::CTRL+Qt::Key_2), &gui_tet),  &QShortcut::activated, [&](){panel_tet.show();});
     QApplication::connect(new QShortcut(QKeySequence(Qt::CTRL+Qt::Key_3), &gui_poly), &QShortcut::activated, [&](){panel_poly.show();});
-
 
     return app.exec();
 }
