0e6606d89c2c31e56a27adab0a4e302f113fae09
P.E.Mazniker <60963077+goodengineer@users.noreply.github.com>
2020-09-14 11:14:24 +0300

    Update main.cpp

--- examples/20_coarse_quad_layouts/main.cpp
+++ examples/20_coarse_quad_layouts/main.cpp
@@ -15,14 +15,14 @@
 #include <cinolib/drawable_sphere.h>
 
 //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+using namespace cinolib;
+using namespace std;
 
 int main(int argc, char **argv)
 {
-    using namespace cinolib;
-
     QApplication a(argc, argv);
 
-    std::string s = (argc==2) ? std::string(argv[1]) : std::string(DATA_PATH) + "cubespikes.obj";
+    string s = (argc==2) ? string(argv[1]) : string(DATA_PATH) + "cubespikes.obj";
     DrawableQuadmesh<> m(s.c_str());
 
     Profiler profiler;
@@ -39,10 +39,8 @@ int main(int argc, char **argv)
     gui.show();
 
     for(uint vid=0; vid<m.num_verts(); ++vid)
-    {
         if(m.vert_is_singular(vid)) gui.push_obj(new DrawableSphere(m.vert(vid), 0.5));
-    }
-
+    
     // CMD+1 to show mesh controls.
     SurfaceMeshControlPanel<DrawableQuadmesh<>> panel(&m, &gui);
     QApplication::connect(new QShortcut(QKeySequence(Qt::CTRL+Qt::Key_1), &gui), &QShortcut::activated, [&](){panel.show();});
