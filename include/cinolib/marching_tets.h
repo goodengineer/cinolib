4022cc8e535802f534dc7ed04421f698190f1c8d
P.M <60963077+goodengineer@users.noreply.github.com>
2020-09-18 15:36:49 +0300

    Update marching_tets.h

--- include/cinolib/marching_tets.h
+++ include/cinolib/marching_tets.h
@@ -50,9 +50,9 @@ template<class M, class V, class E, class F, class P>
 CINO_INLINE
 void marching_tets(const Tetmesh<M,V,E,F,P> & m,
                    const float               isovalue,
-                   std::vector<vec3d>       & verts,
+                   std::vector<vec3f>       & verts,
                    std::vector<uint>        & tris,
-                   std::vector<vec3d>       & norms);
+                   std::vector<vec3f>       & norms);
 }
 
 #ifndef  CINO_STATIC_LIB
